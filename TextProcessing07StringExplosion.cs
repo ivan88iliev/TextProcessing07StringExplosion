dacf0bd321a9b60296ad5e7880da9439f26e6e34
Ivan Iliev <73687077+ivan88iliev@users.noreply.github.com>
2021-03-23 17:47:10 +0200

    Add files via upload

---
+++ TextProcessing07StringExplosion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextProcessing07StringExplosion
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            //TextProcessing07StringExplosion
+
+            string text = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+
+            int power = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char bomb = '>';
+                if (text[i] == bomb)
+                {
+                    power += int.Parse(text[i + 1].ToString());
+                }
+                if (power > 0 && text[i] != bomb)
+                {
+                    power -= 1;
+                    continue;
+                }
+
+                output.Append(text[i]);
+
+            }
+            Console.WriteLine(output);
+
+            // abv>1>1>2>2asdasd
+
+            //
+        }
+    }
+}
